b9138f6991f7f250e8069463ebeafb60466a7660
Joel Peckham <joelskyler@gmail.com>
2021-10-31 14:44:49 -0400

    changed index.php for no reason

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joel Peckham Homework</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/jpeckham/index.php">Joel Peckham's Homework</a>
+</nav>
+<div class="wrapper">
+    <div class="container">
+        <!-- Make a grid of cards. One card per homework assignment. -->
+        <div class="row my-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW01'>Homework 1</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW02'>Homework 2</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW03'>Homework 3</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row py-1">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW04'>Homework 4</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>  
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW05'>Homework 5</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW06'>Homework 6</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW07'>Homework 7</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW08'>Homework 8</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW09'>Homework 9</a></h5>
+                        <p class="card-text text-danger">Ungraded | Score: None</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW10'>Homework 10</a></h5>
+                        <p class="card-text text-danger">Ungraded | Score: None</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW11'>Homework 11</a></h5>
+                        <p class="card-text text-danger">Ungraded | Score: None</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW12'>Homework 12</a></h5>
+                        <p class="card-text text-danger">Ungraded | Score: None</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW13'>Homework 13</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW14'>Homework 14</a></h5>
+                        <p class="card-text">Graded | Score: 100%</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href='/jpeckham/HW15'>Homework 15</a></h5>
+                        <p class="card-text text-danger">Ungraded | Score: None</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
